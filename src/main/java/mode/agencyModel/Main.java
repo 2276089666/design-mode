d49ce3d11f4121c1910cd58d471400dc198c6841
ws <56677272+2276089666@users.noreply.github.com>
2021-05-06 22:38:20 +0800

    代理模式

---
+++ src/main/java/mode/agencyModel/Main.java
@@ -0,0 +1,13 @@
+package mode.agencyModel;
+
+/**
+ * @Author ws
+ * @Date 2021/5/6 22:36
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        Proxy proxy = new Proxy();
+        proxy.request();
+    }
+}
