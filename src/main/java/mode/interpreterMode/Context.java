cdab53c0fa12a42243452b5e7e6c48923c8db343
ws <56677272+2276089666@users.noreply.github.com>
2021-05-26 22:58:28 +0800

    解释器模式

---
+++ src/main/java/mode/interpreterMode/Context.java
@@ -0,0 +1,42 @@
+package mode.interpreterMode;
+
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * @Author ws
+ * @Date 2021/5/25 15:02
+ */
+public class Context {
+    private Expression expression;
+
+    public void calculator(String expression) {
+        char[] array = expression.toCharArray();
+        Stack<Expression> stack = new Stack<>();
+        Expression left;
+        Expression right;
+        // 解析
+        for (int i = 0; i < array.length; i++) {
+            switch (array[i]) {
+                case '+':
+                    left = stack.pop();
+                    right = new VarExpression(String.valueOf(array[++i]));
+                    stack.push(new AddExpression(left, right));
+                    break;
+                case '-':
+                    left=stack.pop();
+                    right=new VarExpression(String.valueOf(array[++i]));
+                    stack.push(new SubExpression(left,right));
+                    break;
+                default:
+                    stack.push(new VarExpression(String.valueOf(array[i])));
+                    break;
+            }
+        }
+        this.expression=stack.pop();
+    }
+
+    public int calculate(Map<String,Integer> map){
+        return this.expression.interpret(map);
+    }
+}
---
+++ src/main/java/mode/interpreterMode/SubExpression.java
@@ -0,0 +1,18 @@
+package mode.interpreterMode;
+
+import java.util.Map;
+
+/**
+ * @Author ws
+ * @Date 2021/5/26 20:04
+ */
+public class SubExpression extends SymbolExpression {
+    public SubExpression(Expression left, Expression right) {
+        super(left, right);
+    }
+
+    @Override
+    public int interpret(Map<String, Integer> map) {
+        return left.interpret(map) - right.interpret(map);
+    }
+}
